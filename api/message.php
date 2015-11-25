290807b62ea5aa0b96ef1dec66b4b0ca6c8063b5
Wil Pringle <eulo@wils-air.home>
2015-11-25 20:03:08 +1100

    mandrill hooked up

--- api/message.php
+++ api/message.php
@@ -9,11 +9,8 @@ header('Content-Type: application/json');
 // Get data
 $request_body = file_get_contents('php://input');
 
-echo 'something entered.';
-
 $data = json_decode($request_body, true);
 
-echo $data;
 // Init DB class
 $db = new Db();
 // Check user hasn't recieve 2 emails already
@@ -28,10 +25,12 @@ if (count($result) > 1 && false) {
   die;
 }
 // Insert
-echo 'something entered2.';
-$insert = $db->query("INSERT INTO messages(to_name,to_email,from_name,from_email,message,terms) VALUES(:to_name,:to_email,:from_name,:from_email,:message,:terms)", $data);
+$insert = $db->query("
+  INSERT INTO messages(to_name,to_email,from_name,from_email,message,terms) 
+  VALUES(:to_name,:to_email,:from_name,:from_email,:message,:terms)", 
+$data);
+
 // Create hash and save it
-echo 'something entered3 .';
 if ($insert < 1) {
   echo json_encode(array(
     'success' => false,
@@ -39,58 +38,63 @@ if ($insert < 1) {
   ));
 }
 $last_id = $db->lastInsertId();
-$hash = hash('ripemd160', $last_id);
+$data['hash'] = hash('ripemd160', $last_id);
 $update = $db->query("UPDATE messages SET hash = :hash WHERE id = :id", array(
   'id' => $last_id,
-  'hash' => $hash 
+  'hash' => $data['hash'] 
 ));
 
 if ($update > 0) {
   
-    try {
-      $mandrill = new Mandrill('uL1CKuc5NO25Tdpzv4uxeg');
-      $template_name = 'without_prize';
-      $template_content = array(
-          array(
-              'name' => 'example name',
-              'content' => 'example content'
-          )
+  try {
+    $mandrill = new Mandrill('uL1CKuc5NO25Tdpzv4uxeg');
+    $template_name = 'without_prize';
+    $template_content = array($data); 
+
+    $merge_vars = array();
+
+    if ($prize_won = false ) {
+      $template_name = 'with_prize';
+      $merge_vars[] = array(
+        'name' => 'prize_coupon',
+        'content' => ''
       );
-      $message = array(
-          'html' => '<p>Example HTML content</p>',
-          'text' => 'Example text content',
-          'subject' => 'example subject',
-          'from_email' => 'message.from_email@example.com',
-          'from_name' => 'Example Name',
-          'to' => array(
-              array(
-                  'email' => 'recipient.email@example.com',
-                  'name' => 'Recipient Name',
-                  'type' => 'to'
-              )
-          ),
-          'headers' => array('Reply-To' => 'message.reply@example.com'),
-          'important' => false,
-          'merge' => true,
-          'merge_language' => 'mailchimp',
-          'global_merge_vars' => array(
-              array(
-                  'name' => 'merge1',
-                  'content' => 'merge1 content'
-              )
-          ),
+    }
+
+    foreach ($data as $k => $v) {
+      $merge_vars[] = array(
+        'name' => $k,
+        'content' => $v 
       );
-      $async = false;
-      $ip_pool = null;
-      $send_at = null;
-      $result = $mandrill->messages->sendTemplate($template_name, $template_content, $message, $async, $ip_pool, $send_at);
-      print_r($result);
+    }
+
+    $message = array(
+      'subject' => $data['from_name'] . ' - Christmas Dumberland!',
+      'from_email' => $data['from_email'],
+      'from_name' => $data['from_name'],
+      'to' => array(
+        array(
+          'email' => $data['to_email'],
+          'name' => $data['to_name'],
+          'type' => 'to'
+        )
+      ),
+      'headers' => array('Reply-To' => $data['from_email']),
+      'important' => false,
+      'merge' => true,
+      'merge_language' => 'handlebars',
+      'global_merge_vars' => $merge_vars,
+    );
+    $async = false;
+    $ip_pool = null;
+    $send_at = null;
+    $result = $mandrill->messages->sendTemplate($template_name, $template_content, $message, $async, $ip_pool, $send_at);
 
   } catch(Mandrill_Error $e) {
-      // Mandrill errors are thrown as exceptions
-      echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
-      // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-      throw $e;
+    // Mandrill errors are thrown as exceptions
+    //echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
+    // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    //throw $e;
   }
 }
 
--- api/thank.php
+++ api/thank.php
@@ -21,7 +21,52 @@ $result = $db->query("SELECT * FROM messages WHERE hash = :hash");
 
 if (count($result)) {
   // TODO: Send email
-  echo json_encode($result);
+  $data = array_merge($data, $result[0]);
+  $data['message'] = $message;
+
+  try {
+    $mandrill = new Mandrill('uL1CKuc5NO25Tdpzv4uxeg');
+    $template_name = 'thank_sender';
+    $template_content = array($data); 
+
+    $merge_vars = array();
+    foreach ($data as $k => $v) {
+      $merge_vars[] = array(
+        'name' => $k,
+        'content' => $v 
+      );
+    }
+
+    $message = array(
+      'subject' => $data['from_name'] . ' - Christmas Dumberland!',
+      'from_email' => $data['to_email'],
+      'from_name' => $data['to_name'],
+      'to' => array(
+        array(
+          'email' => $data['from_email'],
+          'name' => $data['from_name'],
+          'type' => 'to'
+        )
+      ),
+      'headers' => array('Reply-To' => $data['to_email']),
+      'important' => false,
+      'merge' => true,
+      'merge_language' => 'handlebars',
+      'global_merge_vars' => $merge_vars,
+    );
+    $async = false;
+    $ip_pool = null;
+    $send_at = null;
+    $result = $mandrill->messages->sendTemplate($template_name, $template_content, $message, $async, $ip_pool, $send_at);
+
+  } catch(Mandrill_Error $e) {
+    // Mandrill errors are thrown as exceptions
+    //echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
+    // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    //throw $e;
+  }
+
+  echo json_encode($data);
   die;
 }
 
